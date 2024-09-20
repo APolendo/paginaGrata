f84565269a7edd9121e59e2b005ecc151d94c1bc
kfskevin <kfskevin5@gmail.com>
2024-09-19 21:30:00 -0600

    Sincronizacion entre clientes y duos.

--- server.js
+++ server.js
@@ -1,55 +1,76 @@
 const express = require('express');
 const http = require('http');
 const socketIo = require('socket.io');
+const path = require('path');
+
 const app = express();
 const server = http.createServer(app);
 const io = socketIo(server);
 
-const PORT = process.env.PORT || 3000;
+const initialImages = [
+    '/imagenes/CS.png',
+    '/imagenes/FG.png',
+    '/imagenes/FN.png',
+    '/imagenes/RL.png',
+    '/imagenes/WZ.png'
+];
 
-app.use(express.static('public'));
+const duoStates = {};
 
-let duos = {};
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, 'public', 'index.html'));
+});
 
 io.on('connection', (socket) => {
-    console.log('Un cliente se ha conectado:', socket.id);
+    let currentDuoId;
 
     socket.on('join duo', (duoId) => {
-        if (!duos[duoId]) {
-            duos[duoId] = [];
+        socket.join(duoId);
+        currentDuoId = duoId;
+
+        if (!duoStates[duoId]) {
+            duoStates[duoId] = {
+                currentIndex: 0,
+                images: shuffleImages([...initialImages]),
+            };
         }
 
-        if (duos[duoId].length < 2) {
-            duos[duoId].push(socket.id);
-            socket.join(duoId);
-            socket.emit('duo joined', duoId);
-            console.log(`Cliente ${socket.id} se unió al dúo ${duoId}`);
-        } else {
-            socket.emit('duo full', duoId);
-            console.log(`Dúo ${duoId} está lleno`);
-        }
+        socket.emit('sync', duoStates[duoId]);
     });
 
     socket.on('next image', (duoId) => {
-        socket.to(duoId).emit('update next image');
+        if (duoStates[duoId]) {
+            duoStates[duoId].currentIndex = (duoStates[duoId].currentIndex + 1) % duoStates[duoId].images.length;
+            io.to(duoId).emit('update image', duoStates[duoId]);
+        }
     });
 
     socket.on('reset colors', (duoId) => {
-        socket.to(duoId).emit('colors reset');
+        if (duoStates[duoId]) {
+            duoStates[duoId].currentIndex = 0;
+            io.to(duoId).emit('reset', duoStates[duoId]);
+        }
     });
 
     socket.on('shuffle images', (duoId) => {
-        socket.to(duoId).emit('images shuffled');
-    });
-
-    socket.on('disconnect', () => {
-        console.log('Un cliente se ha desconectado:', socket.id);
-        for (let duoId in duos) {
-            duos[duoId] = duos[duoId].filter(id => id !== socket.id);
+        if (duoStates[duoId]) {
+            duoStates[duoId].images = shuffleImages(duoStates[duoId].images);
+            duoStates[duoId].currentIndex = 0; 
+            io.to(duoId).emit('shuffled', duoStates[duoId]);
         }
     });
 });
 
-server.listen(PORT, () => {
-    console.log(`Servidor escuchando en http://localhost:${PORT}`);
+function shuffleImages(array) {
+    for (let i = array.length - 1; i > 0; i--) {
+        const j = Math.floor(Math.random() * (i + 1));
+        [array[i], array[j]] = [array[j], array[i]];
+    }
+    return array;
+}
+
+server.listen(3000, () => {
+    console.log('Servidor escuchando en http://localhost:3000');
 });
